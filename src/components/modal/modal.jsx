9ce62eb3c3e33eb88022a230404db25787d488b9
Nikolay Geldyev <nikoejento@gmail.com>
2019-12-23 15:58:15 +0300

    add propTypes for Modal component

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,37 +1,48 @@
 import React from 'react';
-import classNames from 'classnames'
+import PropTypes from 'prop-types'
 
 import Portal from '../portal/portal'
 
-const Modal = (children, className, accepting, onAccept, onCancel, isOpen, ...attrs) => {
-
-    const classes = classNames(
-        'modal',
-        ...className,
-    )
+const Modal = (children, accepting, onAccept, onCancel, isOpen, ...attrs) => {
 
     return (
         <>
             { isOpen &&
                 <Portal>
-                <div className="modal-wrapper" onClick={onCancel}>
-                    <div className={classes} {...attrs}> 
-                        <button className="modal-close-btn" onClick={onCancel}></button>
-                        <div className="modal-inner">
-                            {children}
+                    <div className="modal-wrapper" onClick={onCancel}>
+                        <div className='modal' {...attrs}> 
+                            <button className="modal-close-btn" onClick={onCancel}></button>
+                            <div className="modal-inner">
+                                {children}
+                            </div>
+                            {accepting &&
+                            <>
+                            <button className="modal-accept" onClick={onAccept}></button>
+                            <button className="modal-define" onClick={onCancel}></button>
+                            </>
+                            }
                         </div>
-                        {accepting &&
-                        <>
-                        <button className="modal-accept" onClick={onAccept}></button>
-                        <button className="modal-define" onClick={onCancel}></button>
-                        </>
-                        }
                     </div>
-                </div>
                 </Portal>
             }
         </>
     )
+}
+
+Modal.propTypes = {
+    isOpen: PropTypes.bool, 
+    children: PropTypes.node , 
+    accepting: PropTypes.bool, 
+    onAccept: PropTypes.func, 
+    onCancel: PropTypes.func, 
+}
+
+Modal.defalutProps = {
+    isOpen: false, 
+    children: null, 
+    accepting: false, 
+    onAccept: () => {}, 
+    onCancel: () => {}, 
 }
 
 export default Modal;
