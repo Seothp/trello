4d6dbf93eea9f82c763deacf9a8d08f4aa318d76
Nikolay Geldyev <nikoejento@gmail.com>
2020-01-11 00:25:49 +0300

    add constants, create empty redux-store

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const ADD_TASK = 'ADD_TASK'
+
+export const ADD_LIST = 'ADD_LIST'
+
+export const REMOVE_TASK = 'REMOVE_TASK'
+
+export const REMOVE_LIST = 'REMOVE_LIST'
+
+export const CHECK_TASK = 'CHECK_TASK'
+
+export const MOVE_TASK = 'MOVE_TASK'
