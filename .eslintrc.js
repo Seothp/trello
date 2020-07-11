1d97f3225d7f6bd3ef6dac747d925e2c6ee1b941
Nikolay Geldyev <nikoejento@gmail.com>
2020-07-11 11:48:50 +0300

    and eslint again...

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 'off',
     'react/jsx-filename-extension': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
+    'linebreak-style': 'off'
   },
 };
