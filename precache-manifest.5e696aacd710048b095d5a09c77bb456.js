bc90f109aa2cf8461e82fec7e7431a676ce78117
Nikolay <nikoejento@gmail.com>
2021-02-11 20:21:35 +0300

    Updates

---
+++ precache-manifest.5e696aacd710048b095d5a09c77bb456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0acb3f9d1315a473473ca1a2576b0e",
+    "url": "/trello/index.html"
+  },
+  {
+    "revision": "046a6e7aa455eb473707",
+    "url": "/trello/static/css/main.cdc49aec.chunk.css"
+  },
+  {
+    "revision": "3d8697e662bd0a5fc0b7",
+    "url": "/trello/static/js/2.a3706eb7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/trello/static/js/2.a3706eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046a6e7aa455eb473707",
+    "url": "/trello/static/js/main.5c39490d.chunk.js"
+  },
+  {
+    "revision": "d9c81a61d51222b704f8",
+    "url": "/trello/static/js/runtime-main.2e9ff2ef.js"
+  }
+]);
